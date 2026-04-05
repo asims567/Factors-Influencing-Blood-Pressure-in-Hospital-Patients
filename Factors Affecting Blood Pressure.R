658aa8b6e77d97545feae4f2d664c498c88365f2
asims567 <asimsheikh567@gmail.com>
2026-04-05 19:12:27 -0400

    Update Factors Affecting Blood Pressure.R

--- Factors Affecting Blood Pressure.R
+++ Factors Affecting Blood Pressure.R
@@ -3,13 +3,14 @@ Blood_Pressure <- read_excel("C:/Users/asims/Downloads/Blood Pressure.XLS")
 View(Blood_Pressure)
 
 class(Blood_Pressure)
+[1] "tbl_df"     "tbl"        "data.frame"
 
 ls()
+[1] "Blood_Pressure"
 
-Blood_Pressure$sbp
-
-str(Blood_Pressure)
 colnames(Blood_Pressure)
+[1] "sbp"     "sex"     "married" "smoke"  
+[5] "age"     "weight"  "race"    "BMI"  
 
 Blood_Pressure$sbp <- as.numeric(Blood_Pressure$sbp)
 Blood_Pressure$BMI <- as.numeric(Blood_Pressure$BMI)
@@ -21,14 +22,22 @@ Blood_Pressure$married <- factor(Blood_Pressure$married, labels = c("Yes", "No")
 Blood_Pressure$race <- factor(Blood_Pressure$race, labels = c("white", "asian", "black", "other"))
 
 mean(Blood_Pressure$sbp)
+[1] 125.8
 median(Blood_Pressure$sbp)
+[1] 130
 sd(Blood_Pressure$sbp)
+[1] 13.39205
 
 mean(Blood_Pressure$BMI)
+[1] 28.68717
 median(Blood_Pressure$BMI)
+[1] 26.60114
 sd(Blood_Pressure$BMI)
+[1] 8.855383
 
 table(Blood_Pressure$race)
+white asian black other 
+   40     8     1     1 
 
 library(ggplot2)
 
@@ -55,4 +64,13 @@ ggplot(Blood_Pressure, aes(x = race)) +
   ggtitle("Race Distribution")
 
 summary(Blood_Pressure$sbp)
+ Min. 1st Qu.  Median    Mean 3rd Qu. 
+   67.0   117.8   130.0   125.8   133.8 
+   Max. 
+  140.0 
+
 summary(Blood_Pressure$BMI)
+Min. 1st Qu.  Median    Mean 3rd Qu. 
+  12.09   21.64   26.60   28.69   34.51 
+   Max. 
+  49.88
